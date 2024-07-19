6444a055083dc2e764e05b274bff8492baa1272f
Jeremy Jones <jeremyjones@navapbc.com>
2024-07-19 11:56:04 -0400

    Add non-working files for test website deploys

---
+++ _plugins/setup_git.rb
@@ -0,0 +1,7 @@
+#!/usr/bin/env ruby
+
+# This Jekyll plugin makes sure that git is configured to run git commit hooks
+# from the .githooks dir
+# This is because the .git directory is ignored by github
+# This plugin runs when jekyll is run
+%x(git config --local core.hooksPath .githooks/)
