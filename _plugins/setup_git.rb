23d719170bd5277781299c6529af6dae531e381d
Jeremy Jones <jeremyjones@navapbc.com>
2024-11-07 11:30:02 -0500

    update comment

--- _plugins/setup_git.rb
+++ _plugins/setup_git.rb
@@ -1,8 +1,9 @@
 #!/usr/bin/env ruby
 
-# This Jekyll plugin makes sure that git is configured to run git commit hooks
-# from the .githooks dir
+# This Jekyll plugin makes sure that git is configured to run git commit hooks from the .githooks dir
 # This is because the .git directory is ignored by github and we want .githooks to be part of the codebase.
+
+# To run manually, do: git config --local core.hooksPath .githooks/
 
 # This plugin runs when jekyll is run
 %x(git config --local core.hooksPath .githooks/)
