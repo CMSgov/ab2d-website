5e2633f2ef3c34567f8c88be866de7e38d57a475
Jeremy Jones <jeremyjones@navapbc.com>
2024-11-07 10:27:20 -0500

    change comment for push testing

--- _plugins/setup_git.rb
+++ _plugins/setup_git.rb
@@ -2,7 +2,7 @@
 
 # This Jekyll plugin makes sure that git is configured to run git commit hooks
 # from the .githooks dir
-# This is because the .git directory is ignored by github and we want .githooks to be part of the codebase
+# This is because the .git directory is ignored by github and we want .githooks to be part of the codebase.
 
 # This plugin runs when jekyll is run
 %x(git config --local core.hooksPath .githooks/)
